6395d852efcb79a8df846662a20a752fb146c2c9
Muradhanki <muradhankilinc@gmail.com>
2024-01-13 23:42:22 +0000

    questions added 10

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,103 +1,49 @@
 // list of all questions, choices, and answers
 var questions = [
     {
-        question: "What is the hardest natural substance on planet Earth?",
-        choices: ["Gold", "Diamond", "Platinum", "Copper"],
-        answers: ["Diamond"]
+        title: "Which river is the longest in the United Kingdom?",
+    choices: ["River Thames", "River Severn", "River Mersey", "River Trent"],
+    answer: "River Severn"
     },
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "What is the capital city of Scotland?",
+    choices: ["Belfast", "Edinburgh", "Cardiff", "Glasgow"],
+    answer: "Edinburgh"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "Which famous playwright was born in Stratford-upon-Avon?",
+        choices: ["Charles Dickens", "Oscar Wilde", "William Shakespeare", "Jane Austen"],
+        answer: "William Shakespeare"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "Which monarch is known as the 'Virgin Queen'?",
+        choices: ["Queen Elizabeth I", "Queen Victoria", "Queen Mary", "Queen Anne"],
+        answer: "Queen Elizabeth I"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "What is the currency used in the United Kingdom?",
+        choices: ["Euro", "Pound Sterling", "Dollar", "Franc"],
+        answer: "Pound Sterling"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "Which iconic rock band originated in London and is known for their operatic style?",
+    choices: ["The Rolling Stones", "The Who", "Queen", "The Beatles"],
+    answer: "Queen"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "In which year did the United Kingdom vote to leave the European Union?",
+        choices: ["2016", "2017", "2018", "2019"],
+        answer: "2016"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "Which famous detective resides at 221B Baker Street?",
+        choices: ["Sherlock Holmes", "Hercule Poirot", "Miss Marple", "Philip Marlowe"],
+        answer: "Sherlock Holmes"
     }
     {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
+        title: "What is the official language of Wales?",
+    choices: ["English", "Welsh", "Scottish Gaelic", "Irish"],
+    answer: "Welsh"
     }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        answers: [""]
-    }
-]
+    
+];
