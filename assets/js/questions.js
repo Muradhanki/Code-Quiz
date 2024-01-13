7485886f9aaf4e18a3281f082065b07c0b4d22f9
Muradhanki <muradhankilinc@gmail.com>
2024-01-13 23:37:27 +0000

    questions format created

---
+++ assets/js/questions.js
@@ -0,0 +1,103 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        question: "What is the hardest natural substance on planet Earth?",
+        choices: ["Gold", "Diamond", "Platinum", "Copper"],
+        answers: ["Diamond"]
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answers: [""]
+    }
+]
